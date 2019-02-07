5077ff52fbdd4169dda3e4a76a43413906ac025e
Dávid Lázár <dajo.lazar@gmail.com>
2019-02-07 11:16:23 +0100

    firebase added

--- koderi/src/main.js
+++ koderi/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import * as firebase from 'firebase' 
 
 Vue.config.productionTip = false
+
+var config = {
+  apiKey: "AIzaSyDK5GBRnkV5jv9jOcIOjB97uOk1h0KXlw4",
+  authDomain: "koderikoderom-365dd.firebaseapp.com",
+  databaseURL: "https://koderikoderom-365dd.firebaseio.com",
+  projectId: "koderikoderom-365dd",
+  storageBucket: "koderikoderom-365dd.appspot.com",
+  messagingSenderId: "678888712592"
+};
+firebase.initializeApp(config);
 
 new Vue({
   render: h => h(App),
