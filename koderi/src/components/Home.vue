cd299f2c1c8b258d38f080dbadc6c9cdb255c038
Dávid Lázár <dajo.lazar@gmail.com>
2019-02-08 19:15:28 +0100

    Update Home.vue
    basic layout changes

--- koderi/src/components/Home.vue
+++ koderi/src/components/Home.vue
@@ -1,21 +1,46 @@
 <template>
-  <v-list>
-    <v-list-tile>
-      <v-list-tile-content>
-        <router-link :to="{name:'cv'}">link to cv</router-link>
-      </v-list-tile-content>
-    </v-list-tile>
-    <v-list-tile>
-      <v-list-tile-content>
-        <router-link :to="{name:'form'}">link to form</router-link>
-      </v-list-tile-content>
-    </v-list-tile>
-    <v-list-tile>
-      <v-list-tile-content>
-        <router-link :to="{name:'thankyou'}">link to thank u</router-link>
-      </v-list-tile-content>
-    </v-list-tile>
-  </v-list>
+  <v-card elevation="0">
+    <v-card-title class="display-3">
+      Vyberte si spôsob vyhodnotenia
+    </v-card-title>
+    <v-card-text class="headline">
+      <p>Vaše skúsenosti a prax vieme vyhodnotiť dvomi spôsobmi.<br>Buď nahratím Vášho CV, ktoré Vám prinesie presnejší odhad mzdy, alebo vyplnením krátkeho dotazníka.</p>
+    </v-card-text>
+    <v-card-text>
+      <v-layout row just>
+        <v-flex xs6>
+          <v-card elevation="0">
+            <v-card-title class="display-2">
+              Nahrať CV
+            </v-card-title>
+            <v-card-text>
+              <p>Vaše skúsenosti budú vyhodnotené s najväčšou presnosťou.</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn depressed color="primary" @click="$router.push({name : 'cv'})">
+                Nahrať CV
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+        <v-flex xs6>
+          <v-card elevation="0">
+            <v-card-title class="display-2">
+              Vyplniť dotazník
+            </v-card-title>
+            <v-card-text>
+              <p>Zaberie 2 minúty, no bude vyhodnotený s menšou presnosťou.</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn depressed color="grey lighten-3" @click="$router.push({name : 'form'})">
+                Vyplniť dotazník
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
